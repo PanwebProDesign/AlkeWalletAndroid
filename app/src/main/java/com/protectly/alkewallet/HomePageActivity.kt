a98df5c5569bdfd8f600dc9273880945cc6eb161
Rodrigo Pantoja <panweb.co@gmail.com>
2024-05-02 19:50:50 -0400

    Initial commit

---
+++ app/src/main/java/com/protectly/alkewallet/HomePageActivity.kt
@@ -0,0 +1,50 @@
+package com.protectly.alkewallet
+
+import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class HomePageActivity : AppCompatActivity() {
+
+    lateinit var linearLayoutEmpty : LinearLayout
+    lateinit var linearLayoutTransactions : LinearLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_home_page)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        //id para linear layout
+        linearLayoutEmpty = findViewById(R.id.emptylayout_hp)
+        linearLayoutTransactions = findViewById(R.id.transactionlayout_hp)
+
+        //creamos la variable y recivimos el dato de pantalla anterior
+        val receivedRegister : String = intent.extras?.getString("NEW_USER").orEmpty()
+        //validamos de que pagina viene
+        validatePageRegister(receivedRegister)
+
+
+    }
+
+    private fun validatePageRegister(page : String) {
+        if (page == "newuser" ){
+            Toast.makeText(this,"Registro", Toast.LENGTH_LONG).show()
+            linearLayoutEmpty.visibility = View.VISIBLE
+            linearLayoutTransactions.visibility = View.GONE
+        }
+        else {
+            Toast.makeText(this,"Login", Toast.LENGTH_LONG).show()
+            linearLayoutEmpty.visibility = View.GONE
+            linearLayoutTransactions.visibility = View.VISIBLE
+        }
+    }
+}
